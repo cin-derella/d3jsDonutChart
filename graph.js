60bcc89de3a0c7ce7e9097d5fd6fe21a743ed486
cin-derella <shirley_cinderella@outlook.com>
2019-10-24 21:55:32 +0800

    update tween

--- graph.js
+++ graph.js
@@ -38,7 +38,9 @@ const update = (data)=>{
         .remove();
     
     //handle the current DOM path updates
-    paths.attr('d',arcPath);
+    paths.attr('d',arcPath)
+        .transition().duration(750)
+        .attrTween('d',arcTweenUpdate);
 
 
     paths.enter()
@@ -48,6 +50,7 @@ const update = (data)=>{
             .attr('stroke','#fff')
             .attr('stroke-width',3)
             .attr('fill',d=>color(d.data.name))
+            .each(function(d){this._current = d})
             .transition().duration(750)
                 .attrTween("d",arcTweenEnter);
 };
@@ -92,3 +95,16 @@ const arcTweenExit = (d)=>{
         return arcPath(d);
     }
 };
+
+//use function keyword to allow use of 'this'
+function arcTweenUpdate(d){
+    //interpolate between the two objects
+    var i = d3.interpolate(this._current,d);
+    //update the current prop with new updated data
+    this._current = i(1);
+
+    return function(t){
+        return arcPath(i(t));
+    }
+
+}
